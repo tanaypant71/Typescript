5270bfb861108bbb30a255e2f828095b29cce185
TANAY PANT <tanaypant71@gmail.com>
2023-08-16 22:41:24 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 0;
+if (age < 50) {
+    age += 10;
+}
+//# sourceMappingURL=index.js.map
---
+++ hello-world/src/index.ts
@@ -0,0 +1,3 @@
+let sales = 123_456_789;
+let course: string = 'John';
+let isBeginner: boolean = true;
