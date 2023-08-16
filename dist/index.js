942c1a0b09be6c797c70dfc53c83e8fe5d885a2f
TANAY PANT <tanaypant71@gmail.com>
2023-08-16 22:59:13 +0530

    Tuples

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,19 @@
 "use strict";
-let age = 0;
-if (age < 50) {
-    age += 10;
-}
+//Built-in types
+// let sales = 123_456_789;
+// let course: string = 'John';
+// let isBeginner: boolean = true;
+// let level;
+//====================================================================================================
+//Any type
+// function hello(name: any) {
+//     console.log(`Hello ${name}`);
+// }
+//====================================================================================================
+//Arrays
+// let list1 = [];
+// let list2: number[] = [1, 2, 3];
+//====================================================================================================
+//Tuple
+let person1 = ['John', 22];
 //# sourceMappingURL=index.js.map
